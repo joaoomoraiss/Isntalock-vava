e94feda60fe5d0fb9b5c73f856f3a0277421f1b3
joaoomoraiss <jooaommorais@gmail.com>
2024-03-06 15:02:45 -0300

    instalock humilde
    a

--- instalock/instalock-neon.py
+++ instalock/instalock-neon.py
@@ -4,10 +4,10 @@ import time
 time_limite = 10
 time_inicial = time.time()
 while True:
-    pyautogui.click(958,916,duration=0.2)
-    sleep=0.2
-    pyautogui.click(960,727,duration=0.2)
-    sleep=0.2
+    pyautogui.click(958,916)
+    sleep=0.3
+    pyautogui.click(960,727)
+    sleep=0.3
     time_atual = time.time()
     if time_atual - time_inicial >= time_limite:
         break
